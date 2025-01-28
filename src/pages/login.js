74824407e69928883cbedada07ef985dd33054e9
Porramin Boonnak <mosphonez3@gmail.com>
2025-01-28 13:01:45 +0700

    new design signup

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,36 +1,7 @@
-import '../pagescss/Login.css'
-import { FaUserAlt } from "react-icons/fa";
-import { PiLockKeyFill } from "react-icons/pi";
-import { useRef } from 'react'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-export default function Login(){
-    const navigate = useNavigate();
-    const username = useRef(null);
-    const password = useRef(null);
-    const url = "https://se-servise.azurewebsites.net/login";
-    const hendelclick=()=>{
-        const data = {
-            username:username.current.value,
-            password:password.current.value
-        }
-        axios.post(url,data).then(response => navigate('/')).catch(error=>{
-            alert("username หรือ password ไม่ถูกต้อง")
-        });
-    }
+export default function Login() {
     return (
-        <div id="main">
-            <div id='menu'>
-                <div id='pic'></div>
-                <h1 id='signup'>
-                    Sign in
-                </h1>
-                <input id="username" ref={username} type="text" className='text_input' placeholder='User name'></input>
-                <FaUserAlt className='icon' id='icon_username'/>
-                <input id="password" ref={password} type="password" className='text_input' placeholder='Password'></input>
-                <PiLockKeyFill className='icon' id='icon_password'/>
-                <button onClick={hendelclick}>Sign in</button>
-            </div>
+        <div class="container-fluid d-flex justify-content-center bg-secondary ">
+            
         </div>
     )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,62 +1,28 @@
-import '../pagescss/Login.css'
-import '../pagescss/Signup.css'
-import { FaUserAlt } from "react-icons/fa";
-import { PiLockKeyFill } from "react-icons/pi";
-import { MdAlternateEmail } from "react-icons/md";
-import { FaPhone } from "react-icons/fa6";
-import { useRef } from 'react'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-export default function Signup(){
-    const navigate = useNavigate();
-    const username = useRef(null);
-    const email = useRef(null);
-    const number = useRef(null);
-    const password = useRef(null);
-    const comfirm_password = useRef(null);
-    const url = "https://se-servise.azurewebsites.net/signup";
-    const hendelclick=()=>{
-        const data = {
-            username : username.current.value,
-            email : email.current.value,
-            number : number.current.value,
-            password : password.current.value,
-            citizenID : "dfdfefed"
-        };
-        if(password.current.value===comfirm_password.current.value)
-        {
-            axios.post(url, data).then(response => navigate('/')).catch(error=>{
-                alert("username ซ้ำ")
-            });
-        }
-        else{
-            alert("password ไม่เหมือนกัน")
-        }
-    }
+import "../pagescss/Signup.css";
+export default function Signup() {
     return (
-        <div id="main">
-            <div id='menu'>
-                <div id='pic'></div>
-                <h1 id='signup'>
-                    Sign up
-                </h1>
-                <input id="username" ref={username} type="text" className='text_input' placeholder='User name'></input>
-                <FaUserAlt className='icon' id='icon_username'/>
+        <div className="container-fluid bg-secondary d-flex flex-column justify-content-start align-items-center vh-100 w-100 text-center">
+            <h1 className="my-5 fw-light">Sign up</h1>
 
-                <input id="email" type="email" ref={email} className='text_input' placeholder='email'></input>
-                <MdAlternateEmail className='icon' id='icon_email'/>
-
-                <input id="number" ref={number} type="text" className='text_input' placeholder='Contact Number'></input>
-                <FaPhone className='icon' id='icon_number'/>
-
-                <input id="password" ref={password} type="password" className='text_input' placeholder='Password'></input>
-                <PiLockKeyFill className='icon' id='icon_password'/>
-
-                <input id="comfirm_password" ref={comfirm_password} type="password" className='text_input' placeholder='Comfirm Password'></input>
-                <PiLockKeyFill className='icon' id='icon_comfirm_password'/>
-
-                <button onClick={hendelclick}>Sign in</button>
+            <div className="form-floating my-4 col-5">
+                <input type="email" className="form-control bg-secondary border border-dark" id="floatingInput" placeholder="name@example.com" />
+                <label htmlFor="floatingInput" className="ms-2 ">
+                    <i className="bi bi-envelope me-2 "></i>Email
+                </label>
             </div>
+
+            <h2 className="my-4">or</h2>
+
+            <button className="btn col-5 bg-secondary border border-dark my-4 ">
+                <img src="https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/google-color.png" alt="Google Logo" width="20" height="20" className="me-2" />
+                Continue with Google
+            </button>
+
+            <div className="d-grid gap-2 col-2 my-4">
+                <button className="btn cs-color" type="button">Button</button>
+            </div>
+            <hr className="w-50 border-pink" />
+            <p>Don’t have an account? <a href="#" className="text-pink fw-bold"><br className="my-2"></br>Sign up</a></p>
         </div>
     )
 }

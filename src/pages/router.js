a18c4b220904da5b6465d90f01f1797487f0bb2c
Porramin Boonnak <mosphonez3@gmail.com>
2025-01-30 18:30:58 +0700

    selectpicture v1

--- src/pages/router.js
+++ src/pages/router.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from '../pages/login'
 import Signup from '../pages/signup';
 import Home from '../pages/home'
 import Signin from "./signin";
 import Information from "../pages/information";
 import ForgetPassword from "../pages/forgetpassword";
+import Selectpicture from "../pages/selectpicture";
 export default function Router(){
     return (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/signup" element={<Signup />}/>
-            <Route path="/login" element={<Login />}/>
             <Route path="/signin" element={<Signin />}/>
             <Route path="/information" element={<Information />}/>
             <Route path="/forgetpassword" element={<ForgetPassword />}/>
+            <Route path="/selectpicture" element={<Selectpicture/>}/>
           </Routes>
         </BrowserRouter>
       );

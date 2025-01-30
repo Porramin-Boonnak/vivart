e1a926effa591a294edc3a623ba874b677b8147a
Porramin <mosphonez3@gmail.com.com>
2025-01-31 00:39:32 +0700

    select picture v2

--- src/pages/selectpicture.js
+++ src/pages/selectpicture.js
@@ -1,6 +1,53 @@
 import { Dropdown } from "react-bootstrap";
 import "../pagescss/selectpicture.css";
+import { FaBahtSign } from "react-icons/fa6";
 export default function Selectpicture() {
+    const examplecomment = [
+        { name: "Naruto", comment: "So interesting.", img: "https://www.beartai.com/wp-content/uploads/2024/02/Naruto-1600x840.jpg" },
+        { name: "Sasuke", comment: "Beautiful as hellll!", img: "https://pm1.aminoapps.com/6493/8e7caf892a720f98952caf5f589e2c265458a291_hq.jpg" },
+        { name: "Sakura", comment: "Your idea is fantastic.", img: "https://i.pinimg.com/736x/8a/0e/8d/8a0e8d8762e8790a788d0c84a68f650a.jpg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that avciejawo ivjoriae joiawj;ogi jroaijgar ehguegij sziovfjowie heasuilli.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" },
+        { name: "Kakashi", comment: "My next homework gonnabe like that.", img: "https://i.redd.it/54lk9zp713kc1.jpeg" }
+
+
+    ];
+
+    const Allcomment = ({ items }) => {
+        return (<>
+            <div className="container">
+                <div className="overflow-auto" style={{ maxHeight: "500px" }}>
+                    {items.map((item) => (
+                        <div>
+                            <div className="row m-2">
+                                <div className="c-comment col-2">
+                                    <img className="rounded-circle c-img-comment" src={item.img} />
+                                </div>
+                                <div className="c-comment col-7 h-100">
+                                    <div className="cs-fs">{item.name}</div>
+                                    <div>{item.comment}</div>
+                                </div>
+                                <div className="c-comment col-2">
+
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>);
+    };
     return (
         <>
             <div className="container-fluid p-0">
@@ -72,8 +119,12 @@ export default function Selectpicture() {
                 </div>
                 <div className="row bg-secondary p-3 ">
                     <div className="row">
-                        <div className="col-7 bg-secondary ms-auto p-0">
+                        <div className="col-7 bg-secondary ms-auto p-0 ">
                             <div id="testtest" class="carousel slide" data-bs-ride="carousel">
+                                <div className="c-card-icon">
+                                    <span className="me-2 ">784185</span>
+                                    <i className="bi bi-heart-fill fs-5 text-primary "></i>
+                                </div>
                                 <div class="carousel-indicators">
                                     <button type="button" data-bs-target="#testtest" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                                     <button type="button" data-bs-target="#testtest" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -107,26 +158,55 @@ export default function Selectpicture() {
                                     <div className="fw-bold">SHARE</div>
                                 </div>
                             </div>
-                            
+
                             <div className="text-decoration-underline mt-3 mb-2">
                                 About the work
                             </div>
 
                             <div className="p-4 border border-dark">
-                                
-                            <div className="m-2">
-                                Size {"         150 x 45 cm"}
-                            </div>
-                            <div className="m-2">
-                                Rarity {"           -"}
-                            </div>
-                            <div className="m-2">
-                                Type {"         Hand draw"}
-                            </div>
+
+                                <div className="m-2">
+                                    Size {"         150 x 45 cm"}
+                                </div>
+                                <div className="m-2">
+                                    Rarity {"           -"}
+                                </div>
+                                <div className="m-2">
+                                    Type {"         Hand draw"}
+                                </div>
                             </div>
                         </div>
-                        <div className="col-4 bg-secondary p-2 border border-dark me-auto">
-
+                        <div className="col-4 bg-secondary p-2 border border-dark me-auto ms-auto">
+                            <div className="d-flex align-items-center justify-content-between">
+                                <h1>Marc Quinn</h1>
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bi bi-three-dots fs-2 me-2"></i>
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item d-flex align-items-center justify-content-between" >Delete<i class="bi bi-trash3-fill"></i></a></li>
+                                        <li><a class="dropdown-item d-flex align-items-center justify-content-between" >Edit<i class="bi bi-pencil-square"></i></a></li>
+                                        <li><a class="dropdown-item d-flex align-items-center justify-content-between" >Report<i class="bi bi-flag-fill"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="fw-light">
+                                Floating dragon, 2025 #Dragon
+                            </div>
+                            <div className="mt-5 fw-light">
+                                Imagination dragon on canvas.
+                            </div>
+                            <div className="fw-light">
+                                150 x 45 cm
+                            </div>
+                            <h5 className="text-primary fw-bold fs-2 mt-4"><FaBahtSign />50,000</h5>
+                            <button type="button" class="btn btn-primary btn-lg rounded-pill w-100 text-white">Add to cart</button>
+                            <div className="border border-dark p-1 mt-4 text-center cs-bg-comment mb-0">
+                                Comment
+                            </div>
+                            <div className="cs-bg-allcomment w-100 ">
+                                <Allcomment items={examplecomment} />
+                            </div>
                         </div>
                     </div>
 

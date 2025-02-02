39a77ce17006df1d5b785adee74f276559b4a1c2
Porramin <mosphonez3@gmail.com.com>
2025-02-02 08:33:19 +0700

    component v1

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import "../pagescss/Home.css"
-import { Dropdown } from "react-bootstrap";
+import Searchbar from "../component/searchbar";
 import { FaBahtSign } from "react-icons/fa6";
 export default function Home() {
     const example = [
@@ -42,72 +42,7 @@ export default function Home() {
     };
     return (<>
         <div className="container-fluid p-0">
-            <div className="row">
-                <div className="col-3 bg-secondary p-2 text-center">
-                    <Dropdown>
-                        <Dropdown.Toggle className="btn cs-color-btn rounded-pill border border-dark w-25"><div className="d-none d-lg-inline-block">Assets</div></Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown drop="end">
-                                <Dropdown.Toggle as="div" className="dropdown-item d-flex align-items-center">
-                                    Rarity
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Normal
-                                        <i class="bi bi-fullscreen-exit"></i>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Unique
-                                        <i class="bi bi-star-fill"></i>
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Dropdown drop="end">
-                                <Dropdown.Toggle as="div" className="dropdown-item d-flex align-items-center">
-                                    Type
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Digital
-                                        <i class="bi bi-film"></i>
-                                    </Dropdown.Item>
-
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Hand draw
-                                        <i class="bi bi-palette-fill"></i>
-                                    </Dropdown.Item>
-
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Sculpture
-                                        <i class="bi bi-piggy-bank-fill"></i>
-                                    </Dropdown.Item>
-
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Painting
-                                        <i class="bi bi-brush-fill"></i>
-                                    </Dropdown.Item>
-
-                                    <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                        Photography
-                                        <i class="bi bi-image-fill"></i>
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
-                <div className="col-6 bg-secondary p-2 ">
-                    <div className="d-flex justify-content-center align-items-center">
-                        <input className="form-control rounded-pill rounded-end-0 w-75 d-inline-block cs-color-Search border-end-0 border border-dark" type="search" placeholder="Searching" aria-label="Search" />
-                        <button type="button" className="btn rounded-pill rounded-start-0 cs-color-btn-Search  border-start-0 border border-dark"><i class="bi bi-search"></i></button>
-                    </div>
-                </div>
-                <div className="col-3 bg-secondary p-2">
-                    <div className="d-flex justify-content-end align-items-center me-5">
-                        <button type="button" className="btn btn-secondary"><div className="d-none d-lg-inline-block">Log out</div><i class="bi bi-box-arrow-right ms-2"></i></button>
-                    </div>
-                </div>
-            </div>
+            <Searchbar/>
             <div className="row bg-secondary p-3">
                 <Allpictures items={example} />
             </div>
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,6 +1,6 @@
-import { Dropdown } from "react-bootstrap";
 import "../pagescss/selectpicture.css";
 import { FaBahtSign } from "react-icons/fa6";
+import Searchbar from "../component/searchbar";
 export default function Post() {
     const examplecomment = [
         { name: "Naruto", comment: "So interesting.", img: "https://www.beartai.com/wp-content/uploads/2024/02/Naruto-1600x840.jpg" },
@@ -51,67 +51,7 @@ export default function Post() {
     return (
         <>
             <div className="container-fluid p-0">
-                <div className="row bg-secondary">
-                    <div className="col-3 bg-secondary p-2 text-center">
-                        <Dropdown>
-                            <Dropdown.Toggle className="btn cs-color-btn rounded-pill border border-dark w-25"><div className="d-none d-lg-inline-block">Assets</div></Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <Dropdown drop="end">
-                                    <Dropdown.Toggle as="div" className="dropdown-item d-flex align-items-center">
-                                        Rarity
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Normal
-                                            <i class="bi bi-fullscreen-exit"></i>
-                                        </Dropdown.Item>
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Unique
-                                            <i class="bi bi-star-fill"></i>
-                                        </Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                                <Dropdown drop="end">
-                                    <Dropdown.Toggle as="div" className="dropdown-item d-flex align-items-center">
-                                        Type
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Digital
-                                            <i class="bi bi-film"></i>
-                                        </Dropdown.Item>
-
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Hand draw
-                                            <i class="bi bi-palette-fill"></i>
-                                        </Dropdown.Item>
-
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Sculpture
-                                            <i class="bi bi-piggy-bank-fill"></i>
-                                        </Dropdown.Item>
-
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Painting
-                                            <i class="bi bi-brush-fill"></i>
-                                        </Dropdown.Item>
-
-                                        <Dropdown.Item href="#" className="d-flex align-items-center justify-content-between">
-                                            Photography
-                                            <i class="bi bi-image-fill"></i>
-                                        </Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                    <div className="col-6 bg-secondary p-2 ">
-                        <div className="d-flex justify-content-center align-items-center">
-                            <input className="form-control rounded-pill rounded-end-0 w-75 d-inline-block cs-color-Search border-end-0 border border-dark" type="search" placeholder="Searching" aria-label="Search" />
-                            <button type="button" className="btn rounded-pill rounded-start-0 cs-color-btn-Search  border-start-0 border border-dark"><i class="bi bi-search"></i></button>
-                        </div>
-                    </div>
-                </div>
+                <Searchbar/>
                 <div className="row bg-secondary p-3 ">
                     <div className="row ">
                         <div className="col-12 col-sm-7 bg-secondary p-0 mx-auto">

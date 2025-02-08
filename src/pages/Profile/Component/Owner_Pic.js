494e40db413e5eb45d16d20fba9b6f8bb9c37d49
koonnon3453@gmail.com <koonnon3453@gmail.com>
2025-02-09 01:18:19 +0700

    Profile UI

---
+++ src/pages/Profile/Component/Owner_Pic.js
@@ -0,0 +1,12 @@
+import { FaBahtSign } from "react-icons/fa6";
+import Searchbar from "../component/searchbar";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Owner_Pic() {
+    return (
+        <>
+        </>
+    );
+}

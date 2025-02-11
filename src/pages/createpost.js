cf5083575725e5e0c6a57721683f6ec4a489dfab
Nokia3310Ww <comgamer15@gmail.com>
2025-02-11 20:45:19 +0700

    fix front bg and align

--- src/pages/createpost.js
+++ src/pages/createpost.js
@@ -1,12 +1,13 @@
 import "../pagescss/createpost.css";
 import Navbar from "../component/navbar"
+import "../pagescss/createpost.css"
 import { IoReturnUpBackOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 export default function Createpost() {
     const navigate = useNavigate();
     return (<>
-        <div className="container-fluid p-0 bg-secondary vh-100">
-        <div className="row">
+        <div className="container-fluid p-0 bg-secondary min-vh-100 min-vw-100">
+            <div className="row">
                 <Navbar />
             </div>
             <div className="row">
@@ -17,14 +18,13 @@ export default function Createpost() {
                     What do you want to post today?
                 </div>
             </div>
-            <div className="row">
-                <div className="row d-flex align-items-center flex-wrap mb-5">
-                    <div className="col-12 col-md-auto text-center text-md-start mb-3 mb-md-0 ">
+            <div className="row d-flex flex-md-row flex-col mb-5 ">
+                <div className="col-12 col-md-1"></div>
+                <div className="col-12 col-md-2 text-center text-md-start mb-3 mb-md-0 ">
                         <div className="fs-2 mb-5" >Normal Post</div>
-                    </div>
-                    <div className="col-12 col-md d-flex flex-wrap justify-content-center justify-content-md-center gap-2">
+                </div>
+                <div className="col-12 col-md-8  d-flex flex-wrap justify-content-center justify-content-lg-start  gap-2">
                         <button className="btn bg-white col-8 col-md-3 me-sm-3 p-5" onClick={()=>navigate("/postnotsale")}>Create Post</button>
-                    </div>
                 </div>
             </div>
             <div className="row">
@@ -32,15 +32,14 @@ export default function Createpost() {
                     <hr className="border-primary border-3 w-100 " />
                 </div>
             </div>
-            <div className="row">
-                <div className="row d-flex align-items-center flex-wrap mb-5">
-                    <div className="col-12 col-md-auto text-center text-md-start mb-3 mb-md-0">
-                        <div className="fs-2 mb-5">Sell Post</div>
-                    </div>
-                    <div className="col-12 col-md d-flex flex-wrap justify-content-center justify-content-md-center gap-2">
-                        <button className="btn bg-white col-8 col-md-3 me-sm-3 p-5" onClick={()=>navigate("/postsaleuniq") }>Unique</button>
-                        <button className="btn bg-white col-8 col-md-3 me-sm-3 p-5" onClick={()=>navigate("/postsaleordinary") }>Ordinary</button>
-                    </div>
+            <div className="row d-flex flex-md-row flex-col mb-5">
+                <div className="col-12 col-md-1"></div>
+                <div className="col-12 col-md-2 text-center text-md-start mb-3 mb-md-0 ">
+                        <div className="fs-2 mb-5" >Sell Post</div>
+                </div>
+                <div className="col-12 col-md-8  d-flex flex-wrap justify-content-center justify-content-lg-start  gap-2">
+                    <button className="btn bg-white col-8 col-md-3 me-sm-3 p-5" onClick={()=>navigate("/postsaleuniq") }>Unique</button>
+                    <button className="btn bg-white col-8 col-md-3 me-sm-3 p-5" onClick={()=>navigate("/postsaleordinary") }>Ordinary</button>
                 </div>
             </div>
 

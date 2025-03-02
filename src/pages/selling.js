85f9fb7354def3772f4c4f419f2dc5e51d680ae0
Sirilux <sirilux.w@ku.th>
2025-03-02 19:54:24 +0700

    fix bug

--- src/pages/selling.js
+++ src/pages/selling.js
@@ -3,11 +3,13 @@ import { IoReturnUpBackOutline } from "react-icons/io5";
 import TuuImage from "../pictures/Tuu.jpg";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useState } from "react";
 
 export default function Selling() {
     const navigate = useNavigate();
     const storedUser = localStorage.getItem("user_login");
     const [loginUser, setLoginUser] = useState(storedUser ? JSON.parse(storedUser) : null);
+
     const forsellerclick = () => {
         navigate("/forseller");
     };
@@ -17,12 +19,15 @@ export default function Selling() {
     };
 
     const sellProduct = async (id) => {
-       
+        if (!loginUser || !loginUser.username) {
+            alert("User not logged in");
+            return;
+        }
 
         try {
             const response = await axios.post("http://localhost:5000/sell", {
                 id,
-                loginUser,
+                loginUser: loginUser.username,  // ส่งเฉพาะ username ไป
             });
 
             alert(`Product ${id} sold successfully!`);

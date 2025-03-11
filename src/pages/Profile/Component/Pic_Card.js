c092a48ebe9dfb8586ef2feac4e9c01cc2e060e1
Sirilux <sirilux.w@ku.th>
2025-03-11 14:18:34 +0700

    edit pic_card profile

--- src/pages/Profile/Component/Pic_Card.js
+++ src/pages/Profile/Component/Pic_Card.js
@@ -1,37 +1,25 @@
-import { FaBahtSign } from "react-icons/fa6";
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from "react";
-import axios from "axios";
+import "../../../pagescss/Pic_Card.css";
 
-export default function Pic_Card({ posts }) {  // Destructure 'posts' from props
+export default function Pic_Card({ posts }) {
     return (
-        <div className="container mt-4">
-            <div className="row">
-                {/* Ensure posts is an array before mapping */}
+        <div className="container-fluid px-5 mt-4"> 
+            <div className="masonry-layout">
                 {(posts || []).map((post, index) => (
-                   <div className="col-md-4 mb-4" key={index}>
-                   <div className="card" style={{ height: "100%", position: "relative" }}>
-                   <i 
-            className="fa fa-cash position-absolute" 
-            style={{ fontSize: "40px", color: "white", top: "10px", right: "10px", zIndex: 10 }}
-        >HEKKI</i>
-                       <img 
-                           src={post.img} 
-                           className="card-img-top" 
-                           alt={post.name} 
-                           style={{ height: "250px", objectFit: "cover" }} 
-                       />
-     
-                       <div className="card-body">
-                           <h5 className="card-title">{post.name}</h5>
-                           <p className="card-text">{post.post || "No comment provided."}</p>
-                       </div>
-                   </div>
-               </div>
-               
+                    <div key={index} className="masonry-item">
+                        <div className="card">
+                            <img 
+                                src={post.img} 
+                                alt={post.name} 
+                                className="card-img-top" 
+                            />
+                            <div className="card-body text-left">
+                                <h5 className="card-title">{post.name}</h5>
+                                <p className="card-text d-inline">{post.post || "No comment provided."}</p>
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
     );
 }
-

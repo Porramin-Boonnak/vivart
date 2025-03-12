8e2357ed3b7053e7f967a0a2cff4083118b129ce
Nonthakan Pimsan <87963315+nxnless@users.noreply.github.com>
2025-03-12 21:05:53 +0700

    noti_ update

--- src/component/notificate_func.js
+++ src/component/notificate_func.js
@@ -68,11 +68,94 @@ const messageFromEachStage = ( data , stage_noti) => {
 
 const PathFromEachStage = (data , stage_noti) =>{
 
+  if (stage_noti == "11") {
+        return "post/"+data.post_id
+  }
+  if (stage_noti == "12") {
+        return "post/"+data.post_id
+  }
+  if (stage_noti == "21") {
+    return data.descript + " Your payment for order " + data.post_msg + "is complete."
+  }
+  if (stage_noti == "22") {
+    return data.descript + " Your artist is packing your order " + data.post_msg + "."
+  }
+  if (stage_noti == "23") {
+    return data.descript + " Your order " + data.post_msg + "is shipping."
+  }
+  if (stage_noti == "24") {
+    return data.descript + " Your shipment for order " + data.post_msg + "has arrived." + "\n\"" + "at" + data.time
+  }
+  if (stage_noti == "25") {
+    return data.descript + " Please pay your order" + data.post_msg + "within 24 hours or it will be canceled."
+  }
+  if (stage_noti == "26") {
+    return data.descript + " Your order" + data.post_msg + "has been canceled." + "\n\"" + "due to your late payment."
+  }
+  if (stage_noti == "27") {
+    return data.sender + " Congratulation! you won the bid." + "\n\"" + "Please check your cart to continue payment."
+  }
+  if (stage_noti == "31") {
+    return data.descript + " Your product has been sold."
+  }
+  if (stage_noti == "32") {
+    return data.sender + " bid your post to" + data.descript + "Baht." //descript = ราคา
+  }
+  if (stage_noti == "33") {
+    return data.descript + " Your bid post timeout in 1 hour."
+  }
+  if (stage_noti == "34") {
+    return data.descript + " Your bid post is timeout." + "\n\"" + "Please select the person to sell."
+  }
+  if (stage_noti == "35") {
+    return data.descript + " The buyer has succesfully paid."
+  }
 
 }
 
 const IconFromEachStage = (data , stage_noti) =>{
-
+ if (stage_noti == "11") {
+        return "👍"
+      }
+      if (stage_noti == "12") {
+        return "💬"
+      }
+      if (stage_noti == "21") {
+        return "💸"
+      }
+      if (stage_noti == "22") {
+        return "📦"
+      }
+      if (stage_noti == "23") {
+        return "🚚"
+      }
+      if (stage_noti == "24") {
+        return "📦✔"
+      }
+      if (stage_noti == "25") {
+        return "⚠"
+      }
+      if (stage_noti == "26") {
+        return data.descript + " Your order" + data.post_msg + "has been canceled." + "\n\"" + "due to your late payment."
+      }
+      if (stage_noti == "27") {
+        return data.sender + " Congratulation! you won the bid." + "\n\"" + "Please check your cart to continue payment."
+      }
+      if (stage_noti == "31") {
+        return data.descript + " Your product has been sold."
+      }
+      if (stage_noti == "32") {
+        return data.sender + " bid your post to" + data.descript + "Baht." //descript = ราคา
+      }
+      if (stage_noti == "33") {
+        return data.descript + " Your bid post timeout in 1 hour."
+      }
+      if (stage_noti == "34") {
+        return data.descript + " Your bid post is timeout." + "\n\"" + "Please select the person to sell."
+      }
+      if (stage_noti == "35") {
+        return data.descript + " The buyer has succesfully paid."
+      }
 }
 /* 
 call
